d6aef71d97d6d6161fae36b21e17fa5a900f8486
J Hubert <jhprojectsnz@gmail.com>
2023-08-29 11:42:02 +1200

    Update filter stalls function

--- src/Pages/search-results/search-results.jsx
+++ src/Pages/search-results/search-results.jsx
@@ -18,16 +18,16 @@ export default function SearchResults({
   mapDetails,
   setMapDetails,
 }) {
-  //Import all stalls from context
+  // Import all stalls from context
   const { stalls } = useUserContext();
-  //location used to find current pathname and use that for conditional rendering
+  // Location used to find current pathname and use that for conditional rendering
   const location = useLocation();
 
-  //Get google map component as a ref
+  // Get google map component as a ref
   const mapRef = useRef(null);
 
-  //This function is run every time a use pauses after zooming or scrolling the map
-  //Stores the new map bounds to state. This causes a rerender and updates filteredStalls
+  // This function is run every time a use pauses after zooming or scrolling the map
+  // Stores the new map bounds to state. This causes a rerender and updates filteredStalls
   function handleOnIdol() {
     setMapDetails((prev) => ({
       ...prev,
@@ -49,35 +49,10 @@ export default function SearchResults({
     }));
   }
 
-  //Make a array of stalls, first filtered by map bounds and then by user filters
-  //The resulting stalls are used as markers on map and to populate results list
+  // Make a array of stalls, first filtered by map bounds and then by user filters
+  // The resulting stalls are used as markers on map and to populate results list
   const filteredStalls = useMemo(() => {
-    //Make a set of related words to the searched word
-    //The items in a stall are checked against this set
-    const matchTerms = filters.keyword
-      ? new Set([
-          filters.keyword.toLowerCase(),
-          filters.keyword.replace(/s$/, "").toLowerCase(),
-          filters.keyword.concat("s").toLowerCase(),
-          filters.keyword,
-          filters.keyword.replace(/s$/, ""),
-          filters.keyword.concat("s"),
-          filters.keyword
-            .charAt(0)
-            .toUpperCase()
-            .concat(filters.keyword.slice(1)),
-          filters.keyword
-            .charAt(0)
-            .toUpperCase()
-            .concat(filters.keyword.slice(1).replace(/s$/, "")),
-          filters.keyword
-            .charAt(0)
-            .toUpperCase()
-            .concat(filters.keyword.slice(1).concat("s")),
-        ])
-      : null;
-
-    //Filter all stalls to just those within the current map bounds
+    // Filter all stalls to just those within the current map bounds
     const stallsWithinMapBounds = mapDetails.mapBounds
       ? stalls.filter((stall) =>
           mapDetails.mapBounds.contains(
@@ -86,37 +61,54 @@ export default function SearchResults({
         )
       : [];
 
-    //Return stalls filtered by selected filters
+    // Return an array of stalls filtered by user selected filters
     return stallsWithinMapBounds.filter((stall) => {
+      // If Open now filter is selected and stall is closed don't include stall
       if (filters.buttonFilters["Open now"] && !isOpen(stall.openTimes))
         return false;
-
+      // If Items in stock is selected and stall has no stock don't include stall
       if (filters.buttonFilters["Items in stock"] && stall.inStock.length < 1)
         return false;
-
+      // If Organic/Market stall/Eftopos payment filters are selected
+      // Check if the stall matchs and exclude if not
       if (filters.buttonFilters.Organic && !stall.organic) return false;
-
       if (filters.buttonFilters["Market stall"] && !stall.marketStall)
         return false;
-
       if (filters.buttonFilters["Eftpos payment"] && !stall.eftposPayment)
         return false;
 
+      // If the user has search for a fruit or vegetable filter stalls by this keyword
       if (filters.keyword) {
-        let hasSearchTerm = false;
-        stall.inStock.forEach((item) => {
-          if (matchTerms.has(item.item)) {
-            hasSearchTerm = true;
+        // Simplify the keyword by:
+        // - Converting to lower case
+        // - Slicing of the end to keyword to account for plurals "ies" or "s"
+        const filterTerm = /ies$/.test(filters.keyword)
+          ? filters.keyword.toLowerCase().slice(0, -3)
+          : filters.keyword.toLowerCase().slice(0, -1);
+
+        // Initiate variable to track whether the stall has an item that matches the filterTerm
+        let stallHasKeyword = false;
+
+        // Look through each product to see if it includes the search term
+        // Sliced filterTerms like cherr (from Cherries) will match Cherries, Cherry etc
+        stall.inStock.forEach((product) => {
+          // toSting needs to be run before toLowerCase will work here
+          // Product.item not stored as a primitive string?
+          const productName = product.item.toString().toLowerCase();
+          if (productName.includes(filterTerm)) {
+            stallHasKeyword = true;
           }
         });
-        if (!hasSearchTerm) return false;
+        // If stallHasKeyword is still false then stall does not have a matching product - don't include
+        if (!stallHasKeyword) return false;
       }
 
+      // If there none of the above conditions have been meet then include the stall in filtered array
       return true;
     });
   });
 
-  //Styles for the Google map - switch of points of interest to make map clearer
+  // Styles for the Google map - switch of points of interest to make map clearer
   const mapStyles = [
     {
       featureType: "poi",
@@ -172,3 +164,16 @@ export default function SearchResults({
     </>
   );
 }
+
+/*
+keyword = strawberries
+
+lowercase = apples
+
+strawberry
+
+app
+
+
+
+*/
