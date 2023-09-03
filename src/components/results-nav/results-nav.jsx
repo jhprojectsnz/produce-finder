8b58852d350c6445a926b67e18034de23c9efb0c
J Hubert <jhprojectsnz@gmail.com>
2023-09-03 20:14:09 +1200

    Fix results nav overlapping results

--- src/components/results-nav/results-nav.jsx
+++ src/components/results-nav/results-nav.jsx
@@ -6,17 +6,23 @@ export default function ResultsNav({ updateMapCenter }) {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const currentPathname = location.pathname;
+  // If the results are shown as list - results nav = position relative. Avoids overlap with results cards
+  // Position of results nav is fixed to bottom of screen for map view
+  const resultsNavSytle =
+    currentPathname === "/results/list" ? "rn-relative" : "rn-fixed";
+
   return (
-    <div className="results-nav">
+    <div className={`results-nav ${resultsNavSytle}`}>
       <ButtonStd
-        appearance={location.pathname === "/results/map" ? "dark" : "light"}
+        appearance={currentPathname === "/results/map" ? "dark" : "light"}
         options={["shadow", "long"]}
         handleClick={() => navigate("/results/map")}
       >
         Map
       </ButtonStd>
       <ButtonStd
-        appearance={location.pathname === "/results/list" ? "dark" : "light"}
+        appearance={currentPathname === "/results/list" ? "dark" : "light"}
         options={["shadow", "long"]}
         handleClick={() => {
           navigate("/results/list");
